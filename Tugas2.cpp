2f9216f0a85cbef37fb13be289a382a8f66944f1
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-09 16:03:55 +0700

    menambah int dan if

--- Tugas2.cpp
+++ Tugas2.cpp
@@ -2,4 +2,10 @@
 using namespace std;
 
 int main(){
-    int x;
+    int x;srand(time(0));
+    x = rand() % 10;
+
+    if (x == 5){
+        x = x + 1;
+        cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
